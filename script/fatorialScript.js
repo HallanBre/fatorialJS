bcd5430fefadccd12caab5163c0a7bba040204cb
HallanBreTa <halllanbandrad@gmail.com>
2023-10-04 19:52:55 -0300

    inputs&PagesFuncionais

--- script/fatorialScript.js
+++ script/fatorialScript.js
@@ -1,16 +1,26 @@
-let n ;
-let fatorial = 1; 
+let fatorial = 1; // Movido para fora do escopo da função de clique
+const btn = document.querySelector("#enviar");
 
-
-function fatCalc(n){
-    
- for (n; n >= 1; n--) {
-    fatorial = fatorial * n;
-    
- }
-  return fatorial;
+function fatCalc(n) {
+    for (n; n >= 1; n--) {
+        fatorial = fatorial * n;
+    }
+    return fatorial;
 }
 
-console.log(fatCalc(5))
 
+ btn.addEventListener("click", function (e) {
+   e.preventDefault();
 
+   const number = document.querySelector("#numb");
+
+   const n = number.value;
+
+   muda_h2(fatCalc(n));
+   fatorial = 1;
+});
+
+function muda_h2(fatorial) {
+   var h2 = document.getElementById('result');
+   h2.innerHTML = fatorial;
+}
